dc69eb82becf4d61b853e683f35642a613aa643e
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-01-23 14:05:30 -0300

    Metodo de salvar e atualizar usuário

--- api/user.js
+++ api/user.js
@@ -1,6 +1,53 @@
+const bcrypt = require('bcrypt-nodejs')
+const { use } = require('passport')
+
 module.exports = app =>{
-    const save = (req, res) =>{
-        res.send('user save')
+    const {existsOrError, notExistsOrError, equalsOrError} = app.api.validations
+
+    const encryptPassWord = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+
+    //Salva e atualiza usuário
+    const save = async (req, res) =>{
+        const user = {...req.body}
+
+        if(req.params.id) user.id = req.params.id
+
+        try {
+            existsOrError(user.name, "Nome não informado")
+            existsOrError(user.email, "E-mail não informado")
+            existsOrError(user.password, "Senha não informada")
+            existsOrError(user.confirmPassWord, "Confrmação de senha não informada")
+            equalsOrError(user.password, user.confirmPassWord, "Senhas não conferem")
+
+            //Verifica se o usuário ja esta cadastrado
+            const userFromDB = await app.db('users')
+                .where({email: user.email}).first()
+            if(!user.id){
+                notExistsOrError(userFromDB, "Usuário já cadastrado")
+            }
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+        user.password = encryptPassWord(user.password)
+        delete user.confirmPassWord
+
+        if(user.id){
+            app.db('users')
+                .update(user)
+                .where({id: user.id})
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500)).send(err)
+        }else{
+            app.db('users')
+                .insert(user)
+                .then(_ => res.status(204).send())
+                .catch(err => err => res.status(500)).send(err)
+        }
+
     }
 
     return {save}
