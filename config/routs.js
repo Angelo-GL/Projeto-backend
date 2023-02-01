d78c943de67136fd6841f46a1a88241a87995477
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-02-01 14:48:36 -0300

    Protegendo rotas com autenticação

--- config/routs.js
+++ config/routs.js
@@ -8,32 +8,40 @@ module.exports = app =>{
     
     //Rotas de Users
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getId)
     
     //Rotas de categoria
     app.route("/categories")
+        .all(app.config.passport.authenticate())
         .post(app.api.category.save)
         .get(app.api.category.get)
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
     app.route("/categories/:id")
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getId)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
     
     //Rotas de Artigos
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .post(app.api.article.save)
         .get(app.api.article.get)
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.article.save)
         .get(app.api.article.getById)
         .delete(app.api.article.remove)
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 
 }
