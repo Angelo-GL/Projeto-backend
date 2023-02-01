3024bf214ebad76a28a2f515aa8f33ab98f69c6c
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-02-01 14:08:24 -0300

    correção de bug em isMath

--- api/auth.js
+++ api/auth.js
@@ -1,26 +1,27 @@
-const {authSecret} = require('../.env')
+const { authSecret } = require('../.env')
 const jwt = require('jwt-simple')
 const bcrypt = require('bcrypt-nodejs')
 
-module.exports = app =>{
+module.exports = app => {
     const signin = async (req, res) => {
-        if(!req.body.email || !req.body.password){
+        if (!req.body.email || !req.body.password) {
             return res.status(400).send('Informe usuário e senha!')
         }
 
         const user = await app.db('users')
-            .where({email: req.body.email})
+            .where({ email: req.body.email })
             .first()
-        
-        if(!user){
+
+        if (!user) {
             return res.status(400).send('Usuário não encontrado!')
         }
 
         //Valida as senhas
-        const isMatch = bcrypt.compareSync(req.body.password, user.password)
-        if(!isMatch){
+        const isMatch = bcrypt.compareSync(req.body.password.toString(), user.password)
+        if (!isMatch) {
             return res.status(401).send('e-mail/senha inválido!')
         }
+
         //Data atual em segundos
         const now = Math.floor(Date.now() / 1000)
 
@@ -30,21 +31,21 @@ module.exports = app =>{
             name: user.name,
             email: user.email,
             iat: now,
-            exp: now + (60 * 60 * 24 * 3) //60 seg, 1(60) min, 24 horas, 3 dias
+            exp: now + (60 * 60 * 24 * 3) //60 seg, 1(60 seg) min, 24 horas, 3 dias
         }
 
-        res.json( {
+        res.json({
             ...payload,
             token: jwt.encode(payload, authSecret)
-        } )
+        })
     }
 
     const validateToken = async (req, res) => {
         const userDate = req.body || null
         try {
-            if(userDate){
+            if (userDate) {
                 const token = jwt.decode(userDate.token, authSecret)
-                if(new Date(token.exp * 100) > new Date()) {
+                if (new Date(token.exp * 100) > new Date()) {
                     return res.send(true)
                 }
             }
@@ -55,5 +56,5 @@ module.exports = app =>{
         res.send(false)
     }
 
-    return {validateToken, signin }
+    return { validateToken, signin }
 }
