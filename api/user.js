03062e9f598a5f6bd6635ccb9bddcd62b6bd00eb
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-02-28 10:51:31 -0300

    Ajustes nas consultas de usuário

--- api/user.js
+++ api/user.js
@@ -39,6 +39,7 @@ module.exports = app =>{
             app.db('users')
                 .update(user)
                 .where({ id: user.id})
+                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500))
         }else{
@@ -54,6 +55,7 @@ module.exports = app =>{
     const get = async (req, res) =>{
         app.db('users')
             .select('id', 'name', 'email', 'admin')
+            .whereNull('deletedAt')
             .then(users => res.json(users))
             .catch(err => res.status(500).send(err))
     }
@@ -64,6 +66,7 @@ module.exports = app =>{
             app.db('users')
                 .select('id', 'name', 'email', 'admin')
                 .where({id: id}).first()
+                .whereNull('deletedAt')
                 .then(user => res.json(user))
                 .catch(err => res.status(500).send(err))
         }else{
