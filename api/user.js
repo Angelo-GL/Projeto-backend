708c4ffaf8b94fcafe7fda91c85320caf0751786
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-02-28 11:25:13 -0300

    Ajuste cadastro de usuário: Validação de cadastro admin

--- api/user.js
+++ api/user.js
@@ -14,6 +14,10 @@ module.exports = app =>{
 
         if(req.params.id) user.id = req.params.id
 
+        //Validações que garantem que o usuário não vai ser cadastrado como admin
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, "Nome não informado")
             existsOrError(user.email, "E-mail não informado")
